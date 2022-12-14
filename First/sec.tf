9ef8def91c0763f69d9f21d0a656546ad8f4f5a2
Nubilajam <102926431+Nubilajam@users.noreply.github.com>
2022-12-14 05:51:21 -0800

    removing HTTPS

--- First/sec.tf
+++ First/sec.tf
@@ -5,13 +5,6 @@ resource "aws_security_group" "wb_sg2" {
   vpc_id      = aws_vpc.VPC_nonprod.id
 
   ingress {
-    description = "HTTPS from everywhere"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
     description = "HTTP from everywhere"
     from_port   = 430
     to_port     = 430
