eeebc9dbe9151b50d28971f4eb76297ad19402cd
Nubilajam <102926431+Nubilajam@users.noreply.github.com>
2022-12-14 06:37:20 -0800

    Update HHTP port number

--- First/main.tf
+++ First/main.tf
@@ -66,8 +66,8 @@ resource "aws_security_group" "web_sg1" {
   
   ingress {
     description = "HTTP from everywhere"
-    from_port   = 430
-    to_port     = 430
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
